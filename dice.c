a93b2a0896e1809a6bdea9e6681d96e160d1ae2f
s1260248 <s1260248@std6dc29.u-aizu.ac.jp>
2019-07-05 18:49:34 +0900

    Won/lost message added

--- dice.c
+++ dice.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-int GetRandom(int min,int max){
-  return min + (int)rand()*(max-min+1.0)/(1.0+RAND_MAX);
-}
+#include <time.h>
 int main(){
+  char name[10];
+  srand(time(NULL));
+  
   int dice1,dice2;
-  dice1 = GetRandom(1,6);
-  dice2 = GetRandom(1,6);
+  printf("What is your name?\n> ");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+
+  printf("Rolling the dice...\n");
+  dice1 = rand() % 6 +1;
+  dice2 = rand() % 6 +1;
+  
   printf("Die 1: %d\n",dice1);
   printf("Die 2: %d\n",dice2);
   printf("Total value: %d\n",dice1+dice2);
+  if(dice1+dice2>=7) printf("%s won!\n",name);
+  else printf("%s lost!\n",name);
   return 0;
 }
 
